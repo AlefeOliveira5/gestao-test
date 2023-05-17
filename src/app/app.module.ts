79c949516743018a88aada8861be5685120451e6
AlefeOliveira5 <lefeoliveira@gmail.com>
2023-05-16 23:05:39 -0300

    subindo atualizações

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
+//IMPORTAÇÃO DOS MODULOS DO PROJETO
 import { AppComponent } from './app.component';
 import { AtendimentoComponent } from './components/atendimento.component';
 import { AcaoComponent } from './components/acao/acao.component';
+import { CursosModule } from './cursos/cursos.module';
 
+
+//Representa os modulos
 @NgModule({
+  //METADADOS
   declarations: [
     AppComponent,
     AtendimentoComponent,
     AcaoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    CursosModule
+
   ],
+  //SERVIÇOS SÃO NOS PROVIDERS TEM ESCOPO GLOBAL
   providers: [],
+  //INSTANCIA O MODULO PRINCIPAL APP.MODULE
   bootstrap: [AppComponent]
+  //FIM DOS METADADOS
 })
+
 export class AppModule { }
